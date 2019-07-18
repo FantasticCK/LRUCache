6e9c1b328af829926b55eaa0a4a34c63bb9a01f0
CK <kainingc@usc.edu>
2019-07-17 23:33:58 -0700

    first commit

---
+++ src/com/CK/Main.java
@@ -0,0 +1,167 @@
+package com.CK;
+
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2 /* capacity */);
+
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1));       // returns 1
+        cache.put(3, 3);    // evicts key 2
+        System.out.println(cache.get(2));       // returns 1
+        cache.put(4, 4);    // evicts key 1
+        System.out.println(cache.get(1));       // returns 1
+        System.out.println(cache.get(3));       // returns 1
+        System.out.println(cache.get(4));       // returns 1
+
+    }
+}
+
+
+class LRUCache {
+    private Queue<int[]> LRU;
+    private int capacity;
+
+    public LRUCache(int capacity) {
+        this.LRU = new LinkedList<>();
+        this.capacity = capacity;
+    }
+
+    public int get(int key) {
+        Iterator<int[]> iterator = LRU.iterator();
+        while (iterator.hasNext()) {
+            int[] next = iterator.next();
+            if (next[0] == key) {
+                LRU.remove(next);
+                LRU.offer(next);
+                return next[1];
+            }
+        }
+        return -1;
+    }
+
+    public void put(int key, int value) {
+        Iterator<int[]> iterator = LRU.iterator();
+        while (iterator.hasNext()) {
+            int[] next = iterator.next();
+            if (next[0] == key) {
+                LRU.remove(next);
+                LRU.offer(new int[]{key, value});
+                return;
+            }
+        }
+        if (!(LRU.size() < capacity)) LRU.poll();
+        LRU.offer(new int[]{key, value});
+    }
+}
+
+// HashTable + DoubledLinkedList
+class LRUCache2 {
+
+    class DLinkedNode {
+        int key;
+        int value;
+        DLinkedNode prev;
+        DLinkedNode next;
+    }
+
+    private void addNode(DLinkedNode node) {
+        /**
+         * Always add the new node right after head.
+         */
+        node.prev = head;
+        node.next = head.next;
+
+        head.next.prev = node;
+        head.next = node;
+    }
+
+    private void removeNode(DLinkedNode node) {
+        /**
+         * Remove an existing node from the linked list.
+         */
+        DLinkedNode prev = node.prev;
+        DLinkedNode next = node.next;
+
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    private void moveToHead(DLinkedNode node) {
+        /**
+         * Move certain node in between to the head.
+         */
+        removeNode(node);
+        addNode(node);
+    }
+
+    private DLinkedNode popTail() {
+        /**
+         * Pop the current tail.
+         */
+        DLinkedNode res = tail.prev;
+        removeNode(res);
+        return res;
+    }
+
+    private Hashtable<Integer, DLinkedNode> cache = new Hashtable<Integer, DLinkedNode>();
+    private int size;
+    private int capacity;
+    private DLinkedNode head, tail;
+
+    public LRUCache2(int capacity) {
+        this.size = 0;
+        this.capacity = capacity;
+
+        head = new DLinkedNode();
+        // head.prev = null;
+
+        tail = new DLinkedNode();
+        // tail.next = null;
+
+        head.next = tail;
+        tail.prev = head;
+    }
+
+    public int get(int key) {
+        DLinkedNode node = cache.get(key);
+        if (node == null) return -1;
+
+        // move the accessed node to the head;
+        moveToHead(node);
+
+        return node.value;
+    }
+
+    public void put(int key, int value) {
+        DLinkedNode node = cache.get(key);
+
+        if (node == null) {
+            DLinkedNode newNode = new DLinkedNode();
+            newNode.key = key;
+            newNode.value = value;
+
+            cache.put(key, newNode);
+            addNode(newNode);
+
+            ++size;
+
+            if (size > capacity) {
+                // pop the tail
+                DLinkedNode tail = popTail();
+                cache.remove(tail.key);
+                --size;
+            }
+        } else {
+            // update the value.
+            node.value = value;
+            moveToHead(node);
+        }
+    }
+}
